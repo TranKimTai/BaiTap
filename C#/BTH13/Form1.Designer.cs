52febf2b28ea231f52d524fefdc4684a3111411e
Trần Kim Tài <147892409+BeGauu@users.noreply.github.com>
2025-12-01 16:16:27 +0700

    Add files via upload

---
+++ C#/BTH13/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace BTH13
+{
+    partial class frmTacgia_Load
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaTG = new System.Windows.Forms.TextBox();
+            this.txtTenTG = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtDienthoaiTG = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(217, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FORM TÁC GIẢ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(38, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã tác giả";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(38, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên tác giả";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label4.Location = new System.Drawing.Point(38, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label5.Location = new System.Drawing.Point(38, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Điện thoại";
+            // 
+            // txtMaTG
+            // 
+            this.txtMaTG.Location = new System.Drawing.Point(135, 56);
+            this.txtMaTG.Name = "txtMaTG";
+            this.txtMaTG.Size = new System.Drawing.Size(290, 20);
+            this.txtMaTG.TabIndex = 2;
+            // 
+            // txtTenTG
+            // 
+            this.txtTenTG.Location = new System.Drawing.Point(135, 94);
+            this.txtTenTG.Name = "txtTenTG";
+            this.txtTenTG.Size = new System.Drawing.Size(290, 20);
+            this.txtTenTG.TabIndex = 2;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(135, 133);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(290, 20);
+            this.txtDiaChi.TabIndex = 2;
+            // 
+            // txtDienthoaiTG
+            // 
+            this.txtDienthoaiTG.Location = new System.Drawing.Point(135, 172);
+            this.txtDienthoaiTG.Name = "txtDienthoaiTG";
+            this.txtDienthoaiTG.Size = new System.Drawing.Size(290, 20);
+            this.txtDienthoaiTG.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.ForeColor = System.Drawing.Color.Navy;
+            this.btnLoad.Location = new System.Drawing.Point(463, 35);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load to Grid";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.ForeColor = System.Drawing.Color.Navy;
+            this.btnThem.Location = new System.Drawing.Point(463, 69);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.ForeColor = System.Drawing.Color.Navy;
+            this.btnLuu.Location = new System.Drawing.Point(463, 107);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 23);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.ForeColor = System.Drawing.Color.Navy;
+            this.btnXoa.Location = new System.Drawing.Point(463, 146);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.ForeColor = System.Drawing.Color.Navy;
+            this.btnThoat.Location = new System.Drawing.Point(463, 185);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(72, 227);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(444, 152);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // frmTacgia_Load
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 431);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtDienthoaiTG);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtTenTG);
+            this.Controls.Add(this.txtMaTG);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTacgia_Load";
+            this.Text = "frmTacgia";
+            this.Load += new System.EventHandler(this.frmTacgia_Load_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaTG;
+        private System.Windows.Forms.TextBox txtTenTG;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtDienthoaiTG;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ C#/BTH13/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient; // Thư viện cung cấp các đối tượng để làm việc với SQL Server
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH13
+{
+    public partial class frmTacgia_Load : Form
+    {
+       
+
+        public frmTacgia_Load()
+        {
+            InitializeComponent(); // Khởi tạo các thành phần giao diện (nút, textbox...)
+        }
+
+        // Sự kiện xảy ra khi Form được tải lên (Load)
+        private void frmTacgia_Load_Load(object sender, EventArgs e)
+        {
+            // Chuỗi kết nối đến cơ sở dữ liệu: Server là máy local (.), Database là LTUD_QLTV
+            string connString = "server=.; database=LTUD_QLTV; Integrated Security =SSPI;";
+
+            // Tạo đối tượng kết nối
+            SqlConnection conn = new SqlConnection(connString);
+
+            // Câu lệnh SQL lấy tất cả dữ liệu từ bảng tblTacgia
+            string sql = "select * from tblTacgia";
+
+            try
+            {
+                conn.Open(); // Mở kết nối đến database
+
+                // Tạo đối tượng thực thi câu lệnh SQL
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text; // Xác định đây là câu lệnh dạng văn bản
+                cmd.CommandText = sql; // Gán câu lệnh SQL
+
+                // Thực thi lệnh Select và trả về bộ đọc dữ liệu (Reader)
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // Gọi hàm hiển thị dữ liệu đọc được
+                hienthiReader(reader);
+            }
+            catch (Exception ex)
+            {
+                // Thông báo lỗi nếu có vấn đề (ví dụ không kết nối được)
+                MessageBox.Show("Lỗi xóa sự kiện!" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close(); // Đóng kết nối để giải phóng tài nguyên (luôn chạy dù lỗi hay không)
+            }
+        }
+
+        // Hàm hiển thị dữ liệu từ SqlDataReader lên Textbox
+        private void hienthiReader(SqlDataReader reader)
+        {
+            // Vòng lặp đọc từng dòng dữ liệu
+            while (reader.Read())
+            {
+                // Gán dữ liệu từ các cột trong database vào các Textbox tương ứng
+                // Lưu ý: Code này sẽ ghi đè liên tục, chỉ hiển thị dòng cuối cùng đọc được
+                this.txtMaTG.Text = reader["MaTG"].ToString();
+                this.txtTenTG.Text = reader["TenTG"].ToString();
+                this.txtDiaChi.Text = reader["Diachi"].ToString();
+                this.txtDienthoaiTG.Text = reader["DienThoai"].ToString();
+            }
+        }
+
+        // Sự kiện khi nhấn nút Thêm (dùng để xóa trắng các ô nhập liệu)
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            setDefault(); // Gọi hàm reset giá trị về rỗng
+        }
+
+        // Hàm đặt các Textbox về trạng thái rỗng
+        private void setDefault()
+        {
+            this.txtMaTG.Text = "";
+            this.txtTenTG.Text = "";
+            this.txtDiaChi.Text = "";
+            this.txtDienthoaiTG.Text = "";
+        }
+
+        // Sự kiện khi nhấn nút Lưu (Thêm mới dữ liệu vào Database)
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu người dùng nhập từ giao diện
+            string matg = this.txtMaTG.Text;
+            string tentg = this.txtTenTG.Text;
+            string diachi = this.txtDiaChi.Text;
+            string dienthoai = this.txtDienthoaiTG.Text;
+
+            // Tạo câu lệnh SQL Insert (Lưu ý: Code gốc dùng 'value' thay vì 'VALUES')
+            string sql = String.Format("Insert into tblTacgia values('{0}',N'{1}',N'{2}','{3}')", matg, tentg, diachi, dienthoai);
+
+            // Khai báo lại chuỗi kết nối và tạo kết nối mới
+            string connString = "server=.; database=LTUD_QLTV;Integrated Security=SSPI;";
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open(); // Mở kết nối
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+
+                // Thực thi câu lệnh Insert (ExecuteNonQuery dùng cho lệnh không trả về dữ liệu)
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Bắt lỗi (thường là lỗi trùng khóa chính hoặc sai cú pháp SQL)
+                MessageBox.Show(matg + "đã có trong CSDL!");
+            }
+            finally
+            {
+                conn.Close(); // Đóng kết nối
+            }
+        }
+
+        // Sự kiện khi nhấn nút Xóa
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            
+
+           
+            // Lấy mã tác giả cần xóa
+            string matg = this.txtMaTG.Text;
+
+            // Tạo câu lệnh SQL 
+
+            string sql = String.Format("Delete from tblTacgia where MaTG ='{0}'", matg);
+
+            string connString = "server=.; database=LTUD_QLTV;Integrated Security=SSPI;";
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.CommandType = CommandType.Text;
+
+                // Thực thi lệnh và lấy số dòng bị ảnh hưởng
+                int kq = cmd.ExecuteNonQuery();
+
+                // Kiểm tra kết quả (kq != -1 nghĩa là thực thi thành công trong một số ngữ cảnh cũ)
+                if (kq != -1)
+                {
+                    MessageBox.Show("đã xóa thành công!" + matg );
+
+                    // Sau khi xóa (hoặc thực hiện lệnh), load lại danh sách để cập nhật giao diện
+                    sql = "select * from tblTacgia";
+                    cmd.CommandText = sql;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    hienthiReader(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(matg + "không xóa thành công!");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        // Sự kiện khi nhấn nút Thoát
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Đóng Form
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // 1. Khai báo chuỗi kết nối
+            string connString = "server=.; database=LTUD_QLTV; Integrated Security =SSPI;";
+            SqlConnection conn = new SqlConnection(connString);
+
+           
+            try
+            {
+                conn.Open();
+                // 2. Câu lệnh SQL lấy dữ liệu
+                string sql = "select * from tblTacgia";
+
+                // 3. Sử dụng SqlDataAdapter để làm cầu nối trung gian
+                // Nó tự động mở kết nối, lấy dữ liệu và đóng kết nối (rất tiện cho Grid)
+                SqlDataAdapter ad = new SqlDataAdapter(sql, conn);
+
+                // 4. Tạo một bảng dữ liệu ảo (DataTable) để chứa dữ liệu lấy về
+                DataTable dt = new DataTable();
+
+                // 5. Đổ dữ liệu từ Adapter vào DataTable
+                ad.Fill(dt);
+
+                // 6. Gán nguồn dữ liệu của DataGridView bằng DataTable vừa có
+                dataGridView.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tải dữ liệu lên lưới: " + ex.Message);
+            }
+            // Với SqlDataAdapter.Fill, ta thường không cần gọi conn.Close() thủ công 
+            // vì nó tự quản lý, nhưng để chắc chắn bạn có thể thêm finally conn.Close() nếu muốn.
+        }
+    }
+}
